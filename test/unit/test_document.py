171210a1750e05283d7f7fe8f2466a6f0dab5a8f
kalaluthien <kalaluthien@gmail.com>
2023-02-12 22:44:50 +0900

    `FoldMap` 이용한 포맷팅 테스트 추가

--- test/unit/test_document.py
+++ test/unit/test_document.py
@@ -65,3 +65,60 @@ def test_fold_map_skip_null() -> None:
 
     document = {"foo": [1, None, 2, None, 3], "bar": None, "baz": 4}
     assert fold_map(document) == {"foo": [1, 2, 3], "baz": 4}
+
+
+def test_fold_map_format() -> None:
+    def format_none(x: None) -> str:
+        return "None"
+
+    def format_bool(x: bool) -> str:
+        return f"Bool({str(x).lower()})"
+
+    def format_int(x: int) -> str:
+        return f"Int({x})"
+
+    def format_float(x: float) -> str:
+        return f"Float({x})"
+
+    def format_str(x: str) -> str:
+        return f"Str({x})"
+
+    def format_list(xs: list[str]) -> str:
+        return f"List({', '.join(xs)})"
+
+    def format_dict(xs: dict[str, str]) -> str:
+        return f"Dict({', '.join(f'{k}: {v}' for k, v in xs.items())})"
+
+    fold_map = FoldMap[str](
+        on_none=format_none,
+        on_bool=format_bool,
+        on_int=format_int,
+        on_float=format_float,
+        on_str=format_str,
+        on_list=format_list,
+        on_dict=format_dict,
+    )
+
+    document: Document = None
+    assert fold_map(document) == "None"
+
+    document = True
+    assert fold_map(document) == "Bool(true)"
+
+    document = 1234
+    assert fold_map(document) == "Int(1234)"
+
+    document = 12.34
+    assert fold_map(document) == "Float(12.34)"
+
+    document = "foo"
+    assert fold_map(document) == "Str(foo)"
+
+    document = [1, 2, 3]
+    assert fold_map(document) == "List(Int(1), Int(2), Int(3))"
+
+    document = {"foo": 1, "bar": 2}
+    assert fold_map(document) == "Dict(foo: Int(1), bar: Int(2))"
+
+    document = {"foo": [1, 2], "bar": 3}
+    assert fold_map(document) == "Dict(foo: List(Int(1), Int(2)), bar: Int(3))"
