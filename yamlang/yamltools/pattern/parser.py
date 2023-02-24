7c83276ac6a0e07c6f80a73b92ee7d5fdb12a23b
kalaluthien <kalaluthien@gmail.com>
2023-02-25 02:13:43 +0900

    Parser 제네릭 추가

--- yamlang/yamltools/pattern/parser.py
+++ yamlang/yamltools/pattern/parser.py
@@ -1,11 +1,16 @@
 from collections.abc import Iterable
+from typing import Generic
+
+from typing_extensions import TypeVar
 
 from yamlang.yamltools import Document, load
 from yamlang.yamltools.pattern.pattern import Pattern
 
+_T = TypeVar("_T", bound=Pattern, default=Pattern, infer_variance=True)
 
-class Parser:
-    def __init__(self, pattern: Pattern) -> None:
+
+class Parser(Generic[_T]):
+    def __init__(self, pattern: _T) -> None:
         self.__pattern = pattern
 
     def parse(self, path: str) -> Iterable[Document]:
