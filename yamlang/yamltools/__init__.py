1174c4195480b6e14286e58e10e740a0c06f5596
kalaluthien <kalaluthien@gmail.com>
2023-02-28 01:39:44 +0900

    `PatternParser` 위치 이전

--- yamlang/yamltools/__init__.py
+++ yamlang/yamltools/__init__.py
@@ -4,9 +4,9 @@ from yamlang.yamltools.document.document import Document  # noqa: F401
 from yamlang.yamltools.document.document import dump  # noqa: F401
 from yamlang.yamltools.document.document import load  # noqa: F401
 from yamlang.yamltools.document.document import patch_yaml_loader  # noqa: F401
+from yamlang.yamltools.parser.parser import PatternParser  # noqa: F401
 from yamlang.yamltools.pattern.container import DictPattern  # noqa: F401
 from yamlang.yamltools.pattern.container import ListPattern  # noqa: F401
-from yamlang.yamltools.pattern.parser import PatternParser  # noqa: F401
 from yamlang.yamltools.pattern.pattern import Pattern  # noqa: F401
 from yamlang.yamltools.pattern.scalar import BoolPattern  # noqa: F401
 from yamlang.yamltools.pattern.scalar import FloatPattern  # noqa: F401
